158af24e8f732777a5efcf7c21e385d7a8450252
Daniel Seripap <daniel@seripap.com>
2021-11-17 13:55:18 -0500

    Just install ssm

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-IFS=',' read -r -a plugins <<< "$INPUT_PLUGINS"
 
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG="${PWD}/kubeconfig"
 
 echo "running entrypoint command(s)"
 
-for i in "${plugins[@]}"
-do
-   helm plugin install $i
-done
 
 response=$(sh -c " $*")
 
